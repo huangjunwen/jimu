ca18c47ebf329aa7d17866f477e952b545446d0a
jayven <kassarar@gmail.com>
2017-09-17 16:26:16 +0800

    Add some note for redis store

---
+++ mw/reftoken/drivers/redis/doc.go
@@ -0,0 +1,3 @@
+// Redis store driver. Redis-server should have version >= 2.6.12 since using "SET key value Ex ttl".
+// Old debian such as wheezy can use backport to install newer version of redis.
+package redis
